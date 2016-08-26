331d5da9d03926bc8d71856230dc5522a054d832
JohnGarden <jrdmnt@gmail.com>
2016-08-26 15:05:33 -0300

    Create form and db to store form information

--- gardenapp/routes/index.js
+++ gardenapp/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* Save form information to database */
+router.post('/myaction', function (req, res) {  
+  router.use(bodyParser.json());
+  const form = req.body;
+  fs.appendFile('db.txt', JSON.stringify({ first_name: form.first_name, last_name: form.last_name }), (err) => {
+      res.send('successfully registered')
+  })
+})
 
 module.exports = router;
